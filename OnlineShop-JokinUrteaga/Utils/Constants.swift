b058c07cd92d3f3eb44ed1562a5a12f6a744c44d
Jokin <jokin.urteaga@ikasle.egibide.org>
2025-03-31 21:22:31 +0200

    Primer commit

---
+++ OnlineShop-JokinUrteaga/Utils/Constants.swift
@@ -0,0 +1,11 @@
+//
+//  Constants.swift
+//  OnlineShop-JokinUrteaga
+//
+//  Created by  on 31/3/25.
+//
+import Foundation
+struct API {
+        static let baseURL = "https://fakestoreapi.com"
+    
+}
