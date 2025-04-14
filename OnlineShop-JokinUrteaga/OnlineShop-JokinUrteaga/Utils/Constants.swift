0d7e0d8a435bd8ce2a8a3df8f94ae457c8c5bd45
JokinU <jokin.urteaga@ikasle.egibide.org>
2025-04-14 18:39:12 +0200

    Cambios desde casa

--- OnlineShop-JokinUrteaga/OnlineShop-JokinUrteaga/Utils/Constants.swift
+++
@@ -1,11 +0,0 @@
-//
-//  Constants.swift
-//  OnlineShop-JokinUrteaga
-//
-//  Created by  on 31/3/25.
-//
-import Foundation
-struct API {
-        static let baseURL = "https://fakestoreapi.com"
-    
-}
